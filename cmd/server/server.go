3c7e21292a493d73672f3addffd79f9912fc14ef
deharahawa <aearahawa@gmail.com>
2021-03-14 22:43:14 -0300

    initial commit

---
+++ cmd/server/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/deharahawa/go-studies/pb"
+	"github.com/deharahawa/go-studies/services"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Could not serve: %v", err)
+	}
+}
