8cfd6d371eeaa60b0e83074f37990759025c3562
deharahawa <aearahawa@gmail.com>
2021-03-18 19:56:03 -0300

    feat(user): add users using client stream

--- services/user.go
+++ services/user.go
@@ -3,6 +3,8 @@ package services
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"time"
 
 	"github.com/deharahawa/go-studies/pb"
@@ -11,6 +13,7 @@ import (
 // type UserServiceServer interface {
 // 	AddUser(context.Context, *User) (*User, error)
 // 	AddUserVerbose(*User, UserService_AddUserVerboseServer) error
+// 	AddUsers(UserService_AddUsersServer) error
 // 	mustEmbedUnimplementedUserServiceServer()
 // }
 
@@ -71,4 +74,30 @@ func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVe
 	time.Sleep(time.Second * 3)
 
 	return nil
+}
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.Users{
+				User: users,
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+		fmt.Println("Adding", req.GetName())
+	}
+
 }
